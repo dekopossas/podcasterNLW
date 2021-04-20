19819aed261f0fb8ed912bd52863147928ab86f1
deko possas <andrepossaster@gmail.com>
2021-04-20 12:27:51 -0300

    importei o sass

--- podcaster-nlw-next/src/pages/_app.tsx
+++ podcaster-nlw-next/src/pages/_app.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import { Header } from '../components/Header';
 import '../styles/global.scss';
 
+import styles from '../styles/app.module.scss';
+
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Header />
       <Component {...pageProps} />
     </div>
