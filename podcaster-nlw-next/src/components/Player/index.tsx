669e6e3496863bdb51a41733e43514fac126c7f6
deko possas <andrepossaster@gmail.com>
2021-04-20 13:18:25 -0300

    tag strong e nova div empty

--- podcaster-nlw-next/src/components/Player/index.tsx
+++ podcaster-nlw-next/src/components/Player/index.tsx
@@ -3,7 +3,14 @@ import styles from './styles.module.scss';
 export function Player() {
   return (
    <div className={styles.playerComponent}>
-     player
+     <header>
+       <img src="/playing.svg" alt="logo do player" />
+       <strong>playing now</strong>
+     </header>
+    <div className={styles.emptyPlayer}>
+      <strong>Selecione um podcast apra ouvir</strong>
+    </div>
+
    </div>
   );
 }
