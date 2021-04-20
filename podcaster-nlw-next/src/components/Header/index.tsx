69b57959cae5a92161b4e9ca2674ee080e771f12
deko possas <andrepossaster@gmail.com>
2021-04-20 11:34:08 -0300

    importado o sass

--- podcaster-nlw-next/src/components/Header/index.tsx
+++ podcaster-nlw-next/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
+import styles from './styles.module.scss';
+
 export function Header() {
   return (
-    <header>
+    <header className={styles.headerContainer}>
       <img src="/logo.svg" alt="logo do podcast" />
 
       <p>O melhor para vc ouvir, sempre</p>
