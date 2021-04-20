a4ac31abb8b560a1118a59a8ad2ac38360a13633
deko possas <andrepossaster@gmail.com>
2021-04-20 11:26:24 -0300

    tag p e span em header

--- podcaster-nlw-next/src/components/Header/index.tsx
+++ podcaster-nlw-next/src/components/Header/index.tsx
@@ -2,6 +2,10 @@ export function Header() {
   return (
     <header>
       <img src="/logo.svg" alt="logo do podcast" />
+
+      <p>O melhor para vc ouvir, sempre</p>
+
+      <span>quinta, 8 abril</span>
     </header>
   );
 }
