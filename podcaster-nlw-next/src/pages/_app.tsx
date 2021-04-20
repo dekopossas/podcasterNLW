4a12c770fe19fc398425f5af0d6e1dfa90dd9c5e
deko possas <andrepossaster@gmail.com>
2021-04-20 12:32:13 -0300

    renderizando o player

--- podcaster-nlw-next/src/pages/_app.tsx
+++ podcaster-nlw-next/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Header } from '../components/Header';
+import { Player } from '../components/Player';
 import '../styles/global.scss';
 
 import styles from '../styles/app.module.scss';
@@ -11,6 +12,7 @@ function MyApp({ Component, pageProps }) {
         <Header />
         <Component {...pageProps} />
       </main>
+      <Player />
     </div>
   )
 }
