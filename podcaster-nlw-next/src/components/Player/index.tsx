07b18c2596d0f373bd634ecd9e8799fced38787b
deko possas <andrepossaster@gmail.com>
2021-04-20 13:41:36 -0300

    barra de progresso

--- podcaster-nlw-next/src/components/Player/index.tsx
+++ podcaster-nlw-next/src/components/Player/index.tsx
@@ -10,7 +10,13 @@ export function Player() {
     <div className={styles.emptyPlayer}>
       <strong>Selecione um podcast apra ouvir</strong>
     </div>
-
+    <footer>
+      <div className={styles.progress}>
+        <span>00:00</span>
+        <div className={styles.emptySlider} />
+      </div>
+      
+    </footer>
    </div>
   );
 }
