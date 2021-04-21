2f73b59419e8596bbed39c1eb652b56cedd2a7ab
deko possas <andrepossaster@gmail.com>
2021-04-21 14:51:34 -0300

    trabalhando tipagens

--- podcaster-nlw-next/src/pages/index.tsx
+++ podcaster-nlw-next/src/pages/index.tsx
@@ -1,13 +1,25 @@
-export default function Home(props) {
+import { GetStaticProps } from 'next';
+
+type Episode = {
+  id: string;
+  title: string;
+  members: string;
+};
+
+type HomeProps = {
+  episodes: Episode[];
+};
+
+export default function Home(props: HomeProps) {
   return (
     <div>
       <h1>Index</h1>
       <p>{JSON.stringify(props.episodes)}</p>
     </div>
   )
-}
+};
 
-export async function getStaticProps() {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await fetch('http://localhost:3333/episodes')
   const data = await response.json();
 
@@ -17,4 +29,4 @@ export async function getStaticProps() {
     },
     revalidate: 60 * 60 * 8,
   }
-}
+};
