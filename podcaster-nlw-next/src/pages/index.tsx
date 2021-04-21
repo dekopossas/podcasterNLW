f7c82dde39442ca638c83f9a2fb82bb7b53482e3
deko possas <andrepossaster@gmail.com>
2021-04-21 19:23:33 -0300

    corrigi o import

--- podcaster-nlw-next/src/pages/index.tsx
+++ podcaster-nlw-next/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticProps } from 'next';
-import { format, parseISO } from 'date-fns'
+import Image from 'next/image';
+import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
@@ -7,15 +8,15 @@ import { convertDurationToTimeString } from '../utils/convertDurationToTimeStrin
 import styles from './home.module.scss';
 
 type Episode = {
-  id: string,
-  title: string,
-  thumbnail: string,
-  members: string,
-  publishedAt: string,
-  duration: number,
-  durationAsString: string,
-  description: string,
-  url: string,
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
 };
 
 type HomeProps = {
@@ -32,7 +33,12 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
           {latesEpisodes.map((episode) => {
             return (
               <li key={episode.id}>
-                <img src={episode.thumbnail} alt={episode.title} />
+                <Image
+                  width={192}
+                  height={192}
+                  src={episode.thumbnail}
+                  alt={episode.title}
+                />
 
                 <div className={styles.episodeDetails}>
                   <a href="">{episode.title}</a>
@@ -44,29 +50,26 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
                   <img src="/play-green.svg" alt="tocar ep" />
                 </button>
               </li>
-            )
+            );
           })}
         </ul>
-
       </section>
 
-      <section className={styles.latesEpisodes}>
-        
-      </section>
+      <section className={styles.latesEpisodes}></section>
     </div>
-  )
-};
+  );
+}
 
 export const getStaticProps: GetStaticProps = async () => {
-  const { data } = await api.get('episodes',{
+  const { data } = await api.get('episodes', {
     params: {
       _limit: 12,
       _sort: 'published_at',
       _order: 'desc',
-    }
-  })
+    },
+  });
 
-  const episodes = data.map(episode => {
+  const episodes = data.map((episode) => {
     return {
       id: episode.id,
       title: episode.title,
@@ -89,5 +92,5 @@ export const getStaticProps: GetStaticProps = async () => {
       allEpisodes,
     },
     revalidate: 60 * 60 * 8,
-  }
+  };
 };
