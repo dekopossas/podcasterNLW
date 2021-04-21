b949b3e2b851674b343b17cf42379e6c8d059521
deko possas <andrepossaster@gmail.com>
2021-04-20 23:23:06 -0300

    css em elementos de progress

--- podcaster-nlw-next/src/components/Player/index.tsx
+++ podcaster-nlw-next/src/components/Player/index.tsx
@@ -7,32 +7,38 @@ export function Player() {
        <img src="/playing.svg" alt="logo do player" />
        <strong>playing now</strong>
      </header>
+
     <div className={styles.emptyPlayer}>
       <strong>Selecione um podcast apra ouvir</strong>
     </div>
-    <footer>
+
+    <footer className={styles.empty}>
       <div className={styles.progress}>
         <span>00:00</span>
         <div className={styles.emptySlider} />
       </div>
+
       <div className={styles.buttons}>
         <button type="button">
           <img src="/shuffle.svg" alt="Embaralhar" />
         </button>
+
         <button type="button">
           <img src="/play-previous.svg" alt="Tocar anterior" />
         </button>
+
         <button type="button">
           <img src="/play.svg" alt="Tocar" />
         </button>
+
         <button type="button">
           <img src="/play-next.svg" alt="Tocar próxima" />
         </button>
+        
         <button type="button">
           <img src="/repeat.svg" alt="Repetir" />
         </button>
       </div>
-
     </footer>
    </div>
   );
