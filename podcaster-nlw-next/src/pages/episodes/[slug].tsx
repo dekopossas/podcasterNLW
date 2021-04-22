263315c6157ff26ea7abc622afab35ea365882e1
deko possas <andrepossaster@gmail.com>
2021-04-22 13:20:32 -0300

    terminei o SSG

--- podcaster-nlw-next/src/pages/episodes/[slug].tsx
+++ podcaster-nlw-next/src/pages/episodes/[slug].tsx
@@ -1,23 +1,61 @@
-import { GetStaticProps } from 'next';
-import { useRouter } from 'next/router';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { GetStaticProps, GetStaticPaths } from 'next';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import { api } from '../../services/api'
 
-export default function Episode() {
-  const router = useRouter();
+type Episode = {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+};
 
+type EpisodeProps = {
+  episode: Episode;
+};
+
+export default function Episode({ episode }: EpisodeProps ) {
   return (
-    <h1>{router.query.slug}</h1>
+    <h1>{episode.title}</h1>
   )
 
 };
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    path: [],
+    fallback: 'blocking'
+  }
+}
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
   const { slug } = ctx.params;
 
   const { data } = await api.get(`/episodes/${slug}`)
 
-  return {
-    props: {},
-    revelidate: 60 * 60 * 24,
-  }
+  const episode = {
+    id: data.id,
+    title: data.title,
+    thumbnail: data.thumbnail,
+    members: data.members,
+    publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+    duration: Number(data.file.duration),
+    durationAsString: convertDurationToTimeString(Number(data.file.duration)),
+    description: data.description,
+    url: data.file.url,
+  };
 }
+
+  return {
+    props: {
+      episode,
+    },
+    revelidate: 60 * 60 * 24, // 24 horas
+  }
+};
