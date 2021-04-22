0ed7f99e1680f02578c8273c4817072b3379f93d
deko possas <andrepossaster@gmail.com>
2021-04-22 12:38:52 -0300

    corrigido importacao e replciado para alleps

--- podcaster-nlw-next/src/pages/index.tsx
+++ podcaster-nlw-next/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
-import { Link } from 'next/link';
+import Link from 'next/link';
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { api } from '../services/api';
@@ -85,7 +85,9 @@ export default function Home({ latesEpisodes, allEpisodes }: HomeProps) {
                     />
                   </td>
                   <td>
-                    <a href="">{episode.title}</a>
+                    <Link href={`/episodes/${episode.id}`}>
+                      <a>{episode.title}</a>
+                    </Link>
                   </td>
                   <td>{episode.members}</td>
                   <td style={{ width: 100 }}>{episode.publishedAt}</td>
