cd0ae52ddb037ab5ef4342b77270b5f40eca8e86
deko possas <andrepossaster@gmail.com>
2021-04-22 22:29:05 -0300

    context aplication

--- podcaster-nlw-next/src/contexts/PlayerContext.ts
+++ podcaster-nlw-next/src/contexts/PlayerContext.ts
@@ -1,3 +1,3 @@
 import { createContext } from 'react';
 
-export const PlayerContext = createContext('deko');
+export const PlayerContext = createContext('');
--- podcaster-nlw-next/src/pages/_app.tsx
+++ podcaster-nlw-next/src/pages/_app.tsx
@@ -2,18 +2,21 @@ import React from 'react';
 import { Header } from '../components/Header';
 import { Player } from '../components/Player';
 import '../styles/global.scss';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from '../styles/app.module.scss';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
+    <PlayerContext.Provider values={'deko'}>
+      <div className={styles.wrapper}>
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContext.Provider>
   )
 }
 
