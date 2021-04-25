9598261cbe992819e4d5eef5f8622dafa524742b
deko possas <andrepossaster@gmail.com>
2021-04-25 13:59:47 -0300

    colocando set isplay no botao

--- podcaster-nlw-next/src/pages/_app.tsx
+++ podcaster-nlw-next/src/pages/_app.tsx
@@ -9,11 +9,12 @@ import styles from '../styles/app.module.scss';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function play(episode) {
     setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0)
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
   }
 
   return (
