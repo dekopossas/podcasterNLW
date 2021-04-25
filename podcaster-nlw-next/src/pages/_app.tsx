c4a5dee8c1eaaaa1ac75c2b920ef493c9a303e2f
deko possas <andrepossaster@gmail.com>
2021-04-25 13:54:45 -0300

    acabei de provar o poder a tipagem criando isPlay

--- podcaster-nlw-next/src/pages/_app.tsx
+++ podcaster-nlw-next/src/pages/_app.tsx
@@ -9,6 +9,7 @@ import styles from '../styles/app.module.scss';
 function MyApp({ Component, pageProps }) {
   const [episodeList, setEpisodeList] = useState([]);
   const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
 
   function play(episode) {
     setEpisodeList([episode]);
@@ -16,7 +17,12 @@ function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play}}>
+    <PlayerContext.Provider value={{
+      episodeList,
+      currentEpisodeIndex,
+      play,
+      isPlaying,
+    }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
